5d7fa8c049ef986ca89d44889600088c8d375b2f
Alexander Weigl <spamling@web.de>
2014-04-17 01:31:14 +0200

    add test case

--- test.py
+++ test.py
@@ -2,17 +2,15 @@ __author__ = 'weigl'
 
 from unittest import TestCase
 
-import gender_guess as gg
 import sys
-
 sys.path.insert(0, "/home/weigl/workspace/gender-guess/build/lib.linux-x86_64-2.7")
+print sys.path
+
+
+import gender_guess as gg
 
 class GenderGuessTest(TestCase):
-    def gender_guess_test(self, name):
-        return
     def test_gender_guess(self):
-        self.assertEqual(gg.guess_gender("Alexander") ,'M')
-        self.assertEqual(gg.guess_gender("Eicke") ,'F')
-
-        self.assertEqual(gg.guess_gender("Alexander") ,'M')
-        self.assertEqual(gg.guess_gender("Eicke") ,'F')
+        self.assertEqual('M', gg.guess_gender("Alexander"))
+        self.assertEqual('?', gg.guess_gender("Eicke"))
+        self.assertEqual('?', gg.guess_gender("Eicke", country= gg.Country.GERMANY))
